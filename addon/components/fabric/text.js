4196332bade236f05ec70553419e33b1d18bcbf2
James Hill <james@onehilltech.com>
2022-11-28 23:29:17 -0500

    chore: added text object

---
+++ addon/components/fabric/text.js
@@ -0,0 +1,23 @@
+import FabricObject from '../../lib/object';
+
+export default class FabricTextComponent extends FabricObject {
+  createObject (fabric) {
+    return new fabric.Text (this.args.text, this.options);
+  }
+
+  get options () {
+    return Object.assign (super.options, FabricObject.compactOptions ({
+      fontFamily: this.args.fontFamily,
+      fontSize: this.args.fontSize,
+      fontStyle: this.args.fontStyle,
+      fontWeight: this.args.fontWeight,
+      underline: this.args.underline,
+      linethrough: this.args.linethrough,
+      overline: this.args.overline,
+      shadow: this.args.shadow,
+      textAlign: this.args.textAlign,
+      textBackgroundColor: this.args.textBackgroundColor,
+      lineHeight: this.args.lineHeight,
+    }));
+  }
+}
---
+++ app/components/fabric/text.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-fabric/components/fabric/text';
