4f4c08235e164772c042fc609e29115594790dc0
James Hill <james@onehilltech.com>
2022-11-29 14:13:37 -0500

    chore: added corner radius to rect

--- addon/components/fabric/rect.js
+++ addon/components/fabric/rect.js
@@ -4,4 +4,11 @@ export default class FabricRectComponent extends FabricObject {
   createObject(fabric) {
     return new fabric.Rect(this.options);
   }
+
+  get options () {
+    return Object.assign (super.options, FabricObject.compactOptions ({
+      rx: this.args.rx,
+      ry: this.args.ry
+    }));
+  }
 }
